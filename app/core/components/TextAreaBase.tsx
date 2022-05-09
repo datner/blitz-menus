655d6d10cbd5787eaf829263cbbf5781f9c5c463
Yuval Datner <contact@datner.me>
2022-05-09 05:31:42 +0300

    feat(components): add TextAreaBase

---
+++ app/core/components/TextAreaBase.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithRef, forwardRef, memo } from "react"
+import clsx from "clsx"
+
+export type TextAreaProps = Omit<ComponentPropsWithRef<"textarea">, "className">
+
+export const TextAreaBase = memo(
+  forwardRef<HTMLTextAreaElement, TextAreaProps>((props, ref) => (
+    <textarea
+      ref={ref}
+      {...props}
+      className={clsx([
+        "appearance-none block w-full px-3 py-2",
+        "border border-gray-300 rounded-md shadow-sm",
+        "placeholder-gray-400",
+        "sm:text-sm",
+        "error:border-red-300 error:focus:ring-red-500 error:text-red-900",
+        "focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none",
+      ])}
+    />
+  ))
+)
