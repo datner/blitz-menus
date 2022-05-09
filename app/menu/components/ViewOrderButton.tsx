bb2bf1d748d007b2b919f9890fe7eb54aca81149
Yuval Datner <contact@datner.me>
2022-05-09 05:34:11 +0300

    refactor(menu): make ViewOrderButton larger

--- app/menu/components/ViewOrderButton.tsx
+++ app/menu/components/ViewOrderButton.tsx
@@ -23,13 +23,13 @@ export function ViewOrderButton(props: Props) {
       show && (
         <animated.button
           style={styles}
-          className="flex fixed inset-x-3 bottom-3 justify-center items-center rounded-md border border-transparent shadow-lg shadow-indigo-300 px-2 py-2 bg-indigo-600 text-base text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+          className="flex fixed inset-x-3 bottom-3 justify-center items-center rounded-md border border-transparent shadow-lg shadow-indigo-300 px-4 py-4 bg-indigo-600 text-base text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
           onClick={onClick}
         >
           <span className="bg-indigo-100 border text-xs border-indigo-500 text-indigo-800 rounded-full h-6 w-6 flex justify-center items-center">
             {amount}
           </span>
-          <span className="inline-block text-left rtl:text-right flex-grow px-3">
+          <span className="inline-block text-left rtl:text-right flex-grow px-4">
             {t("viewOrder")}
           </span>
           <span className="tracking-wider font-thin">₪{price}</span>
