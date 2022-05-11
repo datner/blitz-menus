cbef671b35f4f259635d35e9fe5586b8d2b244c5
Yuval Datner <contact@datner.me>
2022-05-11 05:40:28 +0300

    feat(helpers): add clampBetween helper

--- app/core/helpers/number.ts
+++ app/core/helpers/number.ts
@@ -2,6 +2,8 @@ export function clamp(value: number, min: number, max: number) {
   return Math.min(Math.max(min, value), max)
 }
 
+export const clampBetween = (min: number, max: number) => (value: number) => clamp(value, min, max)
+
 export function sum(values: number[]) {
   return values.reduce((acc, curr) => acc + curr, 0)
 }
