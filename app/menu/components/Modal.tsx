89acc5c51ab741c065d67805d351b8554b7b4da4
Yuval Datner <contact@datner.me>
2022-05-11 05:41:05 +0300

    fix(menu): fix modal not closing on click

--- app/menu/components/Modal.tsx
+++ app/menu/components/Modal.tsx
@@ -25,17 +25,25 @@ export function Modal(props: Props) {
           <a.div
             style={{ opacity: styles.opacity }}
             className="absolute inset-0 max-h-screen bg-gray-500/75"
+            onClick={onClose}
           />
           {/* This element is to trick the browser into centering the modal contents. */}
           <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
             &#8203;
           </span>
-          <a.div style={styles} className="absolute flex flex-col justify-end inset-0 -bottom-12">
-            <div onClick={onClose} className="h-12 flex-shrink-0" />
-            <div className="bg-white flex flex-col relative rounded-t-xl max-h-[calc(100%_-_48px)] pb-12">
+          <a.div
+            style={styles}
+            onClick={onClose}
+            className="absolute flex flex-col justify-end inset-0 -bottom-12"
+          >
+            <div className="h-12 flex-shrink-0" />
+            <div
+              onClick={(event) => event.stopPropagation()}
+              className="bg-white flex flex-col overflow-hidden relative rounded-t-xl max-h-[calc(100%_-_48px)] pb-12"
+            >
               <button
                 onClick={onClose}
-                className="absolute top-2 right-2 z-10 h-10 w-10 p-1.5 text-gray-700 rounded-full bg-gray-400/50"
+                className="absolute top-2 right-2 z-50 h-10 w-10 p-1.5 text-gray-700 rounded-full bg-gray-400/50"
               >
                 <XIcon />
               </button>
