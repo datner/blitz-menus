849e865a4a2a34c5e178690facab4f6855ea7f3a
Yuval Datner <contact@datner.me>
2022-05-11 05:46:55 +0300

    refactor(menu): make button skinny again

--- app/menu/components/OrderModal.tsx
+++ app/menu/components/OrderModal.tsx
@@ -42,7 +42,7 @@ export function OrderModal(props: Props) {
           <div className="h-8" />
           <button
             onClick={onOrder}
-            className="inline-flex w-full justify-center items-center rounded-md border border-transparent shadow-lg shadow-indigo-300 px-4 py-4 bg-indigo-600 text-base text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            className="inline-flex w-full justify-center items-center rounded-md border border-transparent shadow-lg shadow-indigo-300 px-4 py-2 bg-indigo-600 text-base text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
           >
             <span className="bg-indigo-100 border text-xs border-indigo-500 text-indigo-800 rounded-full h-6 w-6 flex justify-center items-center">
               {amount}
