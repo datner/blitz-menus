e3a5e7c574b31c4a3c2ccbe84ec87ab6f1a36509
Yuval Datner <contact@datner.me>
2022-05-23 10:55:24 +0300

    feat(auth): add Impersonate Notice admin

---
+++ app/auth/components/ImpersonationNotice.tsx
@@ -0,0 +1,22 @@
+import { invoke, queryClient, useSession } from "blitz"
+import stopImpersonating from "../mutations/stopImpersonating"
+
+export const ImpersonationNotice = () => {
+  const session = useSession()
+  if (!session.impersonatingFromUserId) return null
+
+  return (
+    <div className="bg-yellow-400 px-2 py-1 text-center font-semibold">
+      <span>Currently impersonating user {session.userId}</span>
+      <button
+        className="appearance-none bg-transparent text-black uppercase"
+        onClick={async () => {
+          await invoke(stopImpersonating, {})
+          queryClient.clear()
+        }}
+      >
+        Exit
+      </button>
+    </div>
+  )
+}
