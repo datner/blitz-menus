f8daab7b0518022a2f84067d578076b3603e268e
Yuval Datner <contact@datner.me>
2022-06-06 00:09:00 +0300

    feat(core): add isExists

--- app/core/helpers/common.ts
+++ app/core/helpers/common.ts
@@ -6,3 +6,7 @@ export const get = <T, V>(option: (as: T) => O.Option<V>, fallback: V) =>
     option,
     O.getOrElse(() => fallback)
   )
+
+export function isExists<T>(val: T | undefined | null): val is T {
+  return val !== undefined && val !== null
+}
