b6ae065df7b70ffa8d9ed40fc040cd01d7d78064
Yuval Datner <contact@datner.me>
2022-06-23 04:57:15 +0300

    refactor(items): extract ensure items exists utils

--- app/items/validations.ts
+++ app/items/validations.ts
@@ -1,45 +1,30 @@
-import { NotFoundError, SessionContext } from "blitz"
-import db, { Locale } from "db"
+import { Locale } from "db"
 import { Slug } from "app/auth/validations"
-import { Id } from "app/core/helpers/zod"
 import { z } from "zod"
-import { ExistsQueryResponse, OwnershipValidator } from "app/auth/helpers/validateOwnership"
-
-async function isItemExists(id: number | undefined, session: SessionContext) {
-  const [{ exists }] =
-    (await db.$queryRaw`SELECT EXISTS(SELECT 1 FROM "Item" WHERE "restaurantId" = ${session.restaurantId} AND id = ${id})`) as ExistsQueryResponse
-
-  return exists
-}
-
-export const ensureItemExists: OwnershipValidator = async (id, session) => {
-  if (!(await isItemExists(id, session))) {
-    throw new NotFoundError(
-      `Could not find item ${id} belonging to restaurant ${session.restaurantId}`
-    )
-  }
-}
 
 export const Content = z.object({
   name: z.string().nonempty(),
   description: z.string().default(""),
 })
 
-export const CreateItem = z.object({
-  id: Id,
-  image: z.string(),
+export const Image = z.object({
+  file: z.any(),
+  src: z.string(),
+  blur: z.string(),
+})
+
+export const ItemSchema = z.object({
+  image: Image,
   price: z.number().int().multipleOf(50, "Price should only be multiples of 50"),
   identifier: Slug,
-  categoryId: Id,
   en: Content.transform((it) => ({ ...it, locale: Locale.en })),
   he: Content.transform((it) => ({ ...it, locale: Locale.he })),
 })
 
-export const UpdateItem = CreateItem.partial()
+export const UpdateItem = ItemSchema
 
-export const UpdateItemSchema = UpdateItem.omit({
-  image: true,
-  id: true,
-  content: true,
-  categoryId: true,
-})
+export const CreateItem = ItemSchema
+
+export type UpdateItem = z.input<typeof UpdateItem>
+export type CreateItem = z.input<typeof CreateItem>
+export type ItemSchema = z.input<typeof ItemSchema>
