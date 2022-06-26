638b8adbd44c5cea0ff947472f317309d2558c4d
Yuval Datner <contact@datner.me>
2022-06-26 05:50:23 +0300

    feat(helpers): add min and max

--- app/core/helpers/number.ts
+++ app/core/helpers/number.ts
@@ -5,6 +5,9 @@ import { Semigroup } from "fp-ts/lib/Semigroup"
 
 export const clamp = O.clamp(N.Ord)
 
+export const min = (first: number) => (second: number) => O.min(N.Ord)(first, second)
+export const max = (first: number) => (second: number) => O.max(N.Ord)(first, second)
+
 export const sum = M.concatAll(N.MonoidSum)
 
 export const divideMonoid: Semigroup<number> = {
