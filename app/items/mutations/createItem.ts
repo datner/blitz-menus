f30b4971f42cbf5e973a1f973e12ef9fca4ed11c
Yuval Datner <contact@datner.me>
2022-07-11 04:10:54 +0300

    fix(item): don't check that the items exists as a condition to create it

--- app/items/mutations/createItem.ts
+++ app/items/mutations/createItem.ts
@@ -2,13 +2,11 @@ import { getUserRestaurant } from "app/auth/helpers/getUserRestaurant"
 import { getBlurDataUrl } from "app/core/helpers/plaiceholder"
 import { resolver } from "blitz"
 import db from "db"
-import { ensureItemExists } from "../ensureItemExists"
 import { CreateItem } from "../validations"
 
 export default resolver.pipe(resolver.zod(CreateItem), resolver.authorize(), async (input, ctx) => {
   const { en, he, image, ...rest } = input
   const restaurant = await getUserRestaurant(ctx)
-  ensureItemExists(input.categoryId, ctx.session)
   const blurDataUrl = await getBlurDataUrl(input.image.src)
   const item = await db.item.create({
     data: {
