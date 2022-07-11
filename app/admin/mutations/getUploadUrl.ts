2a42a202c786bd6fed5873d6eb7ecc51a852d912
Yuval Datner <contact@datner.me>
2022-07-11 04:14:33 +0300

    fix(admin): allow upload for generic restaurant

--- app/admin/mutations/getUploadUrl.ts
+++ app/admin/mutations/getUploadUrl.ts
@@ -1,19 +1,21 @@
 import { resolver } from "blitz"
 import { z } from "zod"
 import { getUserRestaurant } from "app/auth/helpers/getUserRestaurant"
+import { Slug } from "app/auth/validations"
 
 export const UploadImage = z.object({
   name: z.string().nonempty(),
+  restaurant: Slug.optional(),
 })
 
 export default resolver.pipe(
   resolver.zod(UploadImage),
   resolver.authorize(),
-  async ({ name }, ctx) => {
-    const restaurant = await getUserRestaurant(ctx)
+  async ({ name, restaurant }, ctx) => {
+    const slug = restaurant ?? (await getUserRestaurant(ctx)).slug
 
     return {
-      url: `https://api.imgix.com/api/v1/sources/${process.env.IMGIX_SOURCE_ID}/upload/${restaurant.slug}/${name}`,
+      url: `https://api.imgix.com/api/v1/sources/${process.env.IMGIX_SOURCE_ID}/upload/${slug}/${name}`,
       headers: {
         Authorization: `Bearer ${process.env.IMGIX_API_KEY}`,
       },
