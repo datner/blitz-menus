1125014a62ed0f18e90871bdf006da4ebf28204c
Yuval Datner <contact@datner.me>
2022-07-11 04:12:22 +0300

    feat(core): normalize labaled text area

--- app/core/components/LabeledTextArea.tsx
+++ app/core/components/LabeledTextArea.tsx
@@ -1,6 +1,6 @@
 import { ExclamationCircleIcon } from "@heroicons/react/solid"
-import { ComponentPropsWithoutRef, forwardRef, memo } from "react"
-import { FieldError } from "react-hook-form"
+import { ComponentPropsWithoutRef, memo } from "react"
+import { RegisterOptions, useFormContext } from "react-hook-form"
 import { TextAreaBase, TextAreaProps } from "./TextAreaBase"
 
 export interface LabeledTextAreaProps extends TextAreaProps {
@@ -10,44 +10,43 @@ export interface LabeledTextAreaProps extends TextAreaProps {
   label: string
   /** Field type. Doesn't include radio buttons and checkboxes */
   type?: "text" | "password" | "email" | "number"
+  registerOptions?: RegisterOptions
   outerProps?: ComponentPropsWithoutRef<"div">
   labelProps?: ComponentPropsWithoutRef<"label">
-  error?: FieldError
-  errorId?: string
 }
 
-export const LabeledTextArea = memo(
-  forwardRef<HTMLTextAreaElement, LabeledTextAreaProps>(
-    ({ label, error, errorId, outerProps, labelProps, name, ...props }, ref) => {
-      return (
-        <div {...outerProps}>
-          <label {...labelProps}>
-            <span className="block text-sm font-medium text-gray-700">{label}</span>
-            <div className="mt-1 relative">
-              <TextAreaBase
-                ref={ref}
-                name={name}
-                {...props}
-                aria-invalid={Boolean(error)}
-                aria-describedby={error ? errorId : undefined}
-              />
-              {error && (
-                <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                  <ExclamationCircleIcon className="h-5 w-5 text-red-500" aria-hidden="true" />
-                </div>
-              )}
-            </div>
-          </label>
+export const LabeledTextArea = memo<LabeledTextAreaProps>(
+  ({ label, outerProps, labelProps, name, registerOptions, ...props }) => {
+    const { register, getFieldState, formState } = useFormContext()
+    const { error } = getFieldState(name, formState)
+    const errorId = `${name}-error`
+    return (
+      <div {...outerProps}>
+        <label {...labelProps}>
+          <span className="block text-sm font-medium text-gray-700">{label}</span>
+          <div className="mt-1 relative">
+            <TextAreaBase
+              {...register(name, registerOptions)}
+              {...props}
+              aria-invalid={Boolean(error)}
+              aria-describedby={error ? errorId : undefined}
+            />
+            {error && (
+              <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                <ExclamationCircleIcon className="h-5 w-5 text-red-500" aria-hidden="true" />
+              </div>
+            )}
+          </div>
+        </label>
 
-          {error && (
-            <p id={errorId} className="mt-2 text-sm text-red-600">
-              {error.message}
-            </p>
-          )}
-        </div>
-      )
-    }
-  )
+        {error && (
+          <p id={errorId} className="mt-2 text-sm text-red-600">
+            {error.message}
+          </p>
+        )}
+      </div>
+    )
+  }
 )
 
 export default LabeledTextArea
