2d7cb2a98ec540da1f8202ac59664718c66daf4c
Yuval Datner <contact@datner.me>
2022-07-11 04:16:18 +0300

    refactor(admin): normalize create item form to use hooks

--- app/admin/components/CreateItemForm.tsx
+++ app/admin/components/CreateItemForm.tsx
@@ -1,23 +1,12 @@
-import getCategories from "app/categories/queries/getCategories"
-import createItem from "app/items/mutations/createItem"
-import getItem from "app/items/queries/getItem"
-import getItems from "app/items/queries/getItems"
-import { ItemSchema } from "app/items/validations"
-import { useMutation, invalidateQuery, setQueryData, useRouter, Routes } from "blitz"
-import { SubmitHandler } from "react-hook-form"
+import { item } from "app/items/hooks/form"
 import { ItemForm } from "./ItemForm"
 
-export function CreateItemForm() {
-  const [createItemMutation] = useMutation(createItem)
-  const router = useRouter()
+type Props = {
+  redirect?: boolean
+}
 
-  const onSubmit: SubmitHandler<ItemSchema> = async (data) => {
-    const newItem = await createItemMutation(data)
-    setQueryData(getItem, { identifier: newItem.identifier }, newItem)
-    invalidateQuery(getItems)
-    invalidateQuery(getCategories)
-    router.push(Routes.AdminItemsItem({ identifier: newItem.identifier }))
-  }
+export function CreateItemForm(props: Props) {
+  const form = item.useCreate(props)
 
-  return <ItemForm onSubmit={onSubmit} />
+  return <ItemForm {...form} />
 }
