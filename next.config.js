4e8d1b34af2f9c4add08ba67925ec322314e494c
Yuval Datner <contact@datner.me>
2022-07-22 03:45:44 +0300

    feat(tiny): add tiny url support!

--- next.config.js
+++ next.config.js
@@ -1,20 +1,35 @@
+// @ts-check
 const { withBlitz } = require("@blitzjs/next")
 const { withPlaiceholder } = require("@plaiceholder/next")
 const { Locale } = require("@prisma/client")
 
-module.exports = withPlaiceholder(
-  withBlitz({
-    blitz: {},
-    reactStrictMode: true,
-    swcMinify: true,
-    i18n: {
-      locales: Object.values(Locale),
-      defaultLocale: Locale.en,
-    },
-    images: {
-      loader: "imgix",
-      path: "https://renu.imgix.net/",
-      domains: ["renu.imgix.net", ""],
-    },
-  })
-)
+/**
+ * @type {import('next').NextConfig}
+ **/
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: Object.values(Locale),
+    defaultLocale: Locale.en,
+  },
+  images: {
+    loader: "imgix",
+    path: "https://renu.imgix.net/",
+    domains: ["renu.imgix.net", ""],
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/t/:path",
+          destination: "/tiny/:path",
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    }
+  },
+}
+
+module.exports = withPlaiceholder(withBlitz(config))
