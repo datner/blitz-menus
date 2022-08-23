b29a35f69b00d9257a1893133691c5c93e8b5ea3
Yuval Datner <contact@datner.me>
2022-08-23 21:53:12 +0300

    feat(helpers): change to endomorphism

--- app/core/helpers/number.ts
+++ app/core/helpers/number.ts
@@ -1,7 +1,10 @@
 import * as O from "fp-ts/Ord"
+import { fromPredicate, Option } from "fp-ts/Option"
 import * as N from "fp-ts/number"
 import * as M from "fp-ts/Monoid"
-import { Semigroup } from "fp-ts/lib/Semigroup"
+import * as P from "fp-ts/Predicate"
+import { pipe } from "fp-ts/function"
+import { Endomorphism } from "fp-ts/Endomorphism"
 
 export const clamp = O.clamp(N.Ord)
 
@@ -10,10 +13,21 @@ export const max = (first: number) => (second: number) => O.max(N.Ord)(first, se
 
 export const sum = M.concatAll(N.MonoidSum)
 
-export const divideMonoid: Semigroup<number> = {
-  concat: (x, y) => x / y,
-}
+export const divide =
+  (divisor: number): Endomorphism<number> =>
+  (dividend: number) =>
+    dividend / divisor
 
-export const divide = (by: number) => (value: number) => value / by
+export const multiply =
+  (by: number): Endomorphism<number> =>
+  (value: number) =>
+    value * by
 
-export const multiply = (by: number) => (value: number) => value * by
+export const isValid = P.not(Number.isNaN)
+
+export const fromStringWithRadix =
+  (radix: number) =>
+  (str: string): Option<number> =>
+    pipe(Number.parseInt(str, radix), fromPredicate(isValid))
+
+export const fromString = fromStringWithRadix(10)
