f80376af61481d24202563933d0d81a7e484ca64
Yuval Datner <contact@datner.me>
2022-08-23 21:53:27 +0300

    feat(helpers): create prisma fp helpers

---
+++ app/core/helpers/prisma.ts
@@ -0,0 +1,33 @@
+import { Prisma } from "@prisma/client"
+import { PrismaNotFoundError } from "app/core/type/prisma"
+import db from "db"
+import { tryCatch } from "fp-ts/TaskEither"
+
+export const prismaNotFound = (e: unknown): PrismaNotFoundError => ({
+  tag: "prismaNotFoundError",
+  error: e as Prisma.NotFoundError,
+})
+
+export const getVenueById =
+  <Include extends Prisma.VenueInclude>(include: Include) =>
+  (id: number) =>
+    tryCatch(
+      () =>
+        db.venue.findUniqueOrThrow({
+          where: { id },
+          include,
+        }),
+      prismaNotFound
+    )
+
+export const getVenueByIdentifier =
+  <Include extends Prisma.VenueInclude>(include: Include) =>
+  (identifier: string) =>
+    tryCatch(
+      () =>
+        db.venue.findUniqueOrThrow({
+          where: { identifier },
+          include,
+        }),
+      prismaNotFound
+    )
