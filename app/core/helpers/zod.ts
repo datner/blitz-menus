949d6944975ea4183e1170aee6d8c705c16ead3e
Yuval Datner <contact@datner.me>
2022-08-23 21:53:59 +0300

    feat(helpers): add zod fp helpers

--- app/core/helpers/zod.ts
+++ app/core/helpers/zod.ts
@@ -1,3 +1,5 @@
+import { pipe } from "fp-ts/function"
+import * as E from "fp-ts/Either"
 import { z } from "zod"
 
 export const Id = z.number().int().nonnegative()
@@ -23,3 +25,15 @@ export const IdOrSlug = z.union([
   BaseEntity.pick({ id: true }),
   BaseEntity.pick({ identifier: true }),
 ])
+
+type ZodError = {
+  tag: "zodError"
+  error: z.ZodError
+}
+
+export const zodParse =
+  <Schema extends z.ZodTypeAny>(schema: Schema) =>
+  (data: unknown): E.Either<ZodError, z.output<Schema>> =>
+    pipe(schema.safeParse(data), (result) =>
+      result.success ? E.right(result.data) : E.left({ tag: "zodError", error: result.error })
+    )
