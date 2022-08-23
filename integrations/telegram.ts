c83829c9cf4c67c0c139604a2146365dcdb18218
Yuval Datner <contact@datner.me>
2022-08-23 21:55:26 +0300

    feat(integrations): add telegram integrations

--- integrations/telegram.ts
+++ integrations/telegram.ts
@@ -1,12 +1,30 @@
-import { Telegram } from "telegraf"
+import { pipe } from "fp-ts/lib/function"
+import * as TE from "fp-ts/lib/TaskEither"
+import { Telegram, TelegramError } from "telegraf"
+import { Task } from "fp-ts/Task"
+import { log } from "blitz"
 
-export namespace Reporter {
-  const telegram = new Telegram(process.env.TELEGRAM_BOT_TOKEN)
-  const chatId = process.env.TELEGRAM_CHAT_ID
-  export const sendMessage = (msg: string) =>
-    telegram.sendMessage(
-      chatId,
-      process.env.NODE_ENV !== "production" ? "\\[DEVELOPMENT\\]\n\n" + msg : msg,
-      { parse_mode: "MarkdownV2" }
+const telegram = new Telegram(process.env.TELEGRAM_BOT_TOKEN)
+
+const chatId = process.env.TELEGRAM_CHAT_ID
+
+export const sendMessage = (msg: string) =>
+  telegram.sendMessage(
+    chatId,
+    process.env.NODE_ENV !== "production" ? "\\[DEVELOPMENT\\]\n\n" + msg : msg,
+    { parse_mode: "MarkdownV2" }
+  )
+
+const sendMessageFp = (msg: string): Task<void> =>
+  pipe(
+    TE.tryCatch(
+      () => sendMessage(msg),
+      (reason) => reason as TelegramError
+    ),
+    TE.match(
+      (err) => log.error(err.message),
+      () => log.success("Reported to telegram")
     )
-}
+  )
+
+export const fp = { sendMessage: sendMessageFp }
