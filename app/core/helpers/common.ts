e9b1791f4bb6a972f57b1119e58b7ff54a89e1d5
Yuval Datner <contact@datner.me>
2022-08-23 21:52:48 +0300

    feat(helpers): add absurd error

--- app/core/helpers/common.ts
+++ app/core/helpers/common.ts
@@ -1,4 +1,4 @@
-import { flow } from "fp-ts/function"
+import { constant, flow } from "fp-ts/function"
 import * as O from "fp-ts/Option"
 
 export const get = <T, V>(option: (as: T) => O.Option<V>, fallback: V) =>
@@ -10,3 +10,9 @@ export const get = <T, V>(option: (as: T) => O.Option<V>, fallback: V) =>
 export function isExists<T>(val: T | undefined | null): val is T {
   return val !== undefined && val !== null
 }
+
+export type AbsurdError = {
+  tag: "absurdError"
+}
+
+export const constAbsurdError = constant<AbsurdError>({ tag: "absurdError" })
