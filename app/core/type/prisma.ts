9b21631f3c0f26f801926361d0b5ef96d8144b37
Yuval Datner <contact@datner.me>
2022-08-24 13:57:39 +0300

    feat(core): add prisma validation error type

--- app/core/type/prisma.ts
+++ app/core/type/prisma.ts
@@ -4,3 +4,8 @@ export type PrismaNotFoundError = {
   tag: "prismaNotFoundError"
   error: Prisma.NotFoundError
 }
+
+export type PrismaValidationError = {
+  tag: "prismaValidationError"
+  error: Prisma.PrismaClientValidationError
+}
