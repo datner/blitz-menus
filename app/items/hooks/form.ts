9b373359c092ce4da3987a3ee0b28edfba3b4e9f
Yuval Datner <contact@datner.me>
2022-08-24 14:00:36 +0300

    refactor(menu): convert createItem to fp

--- app/items/hooks/form.ts
+++ app/items/hooks/form.ts
@@ -3,13 +3,16 @@ import { Routes } from "@blitzjs/next"
 import { invalidateQuery, setQueryData, useMutation, useQuery } from "@blitzjs/rpc"
 import getCategories from "app/categories/queries/getCategories"
 import { PromiseReturnType } from "blitz"
-import { Locale } from "@prisma/client"
+import { Locale, Prisma } from "@prisma/client"
 import { SubmitHandler } from "react-hook-form"
 import createItem from "../mutations/createItem"
 import updateItem from "../mutations/updateItem"
 import getItem from "../queries/getItem"
 import getItems from "../queries/getItems"
 import { Content, ItemSchema } from "../validations"
+import { pipe } from "fp-ts/function"
+import * as TE from "fp-ts/TaskEither"
+import * as T from "fp-ts/Task"
 
 interface CreateOptions {
   redirect?: boolean
@@ -42,13 +45,24 @@ export const item = {
     const [createItemMutation] = useMutation(createItem)
     const router = useRouter()
 
-    const onSubmit: SubmitHandler<ItemSchema> = async (data) => {
-      const newItem = await createItemMutation(data)
-      setQueryData(getItem, { identifier: newItem.identifier }, newItem)
-      invalidateQuery(getItems)
-      invalidateQuery(getCategories)
-      if (redirect) router.push(Routes.AdminItemsItem({ identifier: newItem.identifier }))
-    }
+    const onSubmit: SubmitHandler<ItemSchema> = (data) =>
+      pipe(
+        () => createItemMutation(data),
+        TE.chainFirstIOK(
+          (item) => () => setQueryData(getItem, { identifier: item.identifier }, item)
+        ),
+        TE.apFirst(TE.of(invalidateQuery(getItems))),
+        TE.apFirst(
+          TE.of(
+            invalidateQuery(getCategories, {
+              orderBy: { identifier: Prisma.SortOrder.asc },
+            })
+          )
+        ),
+        TE.match(console.error, ({ identifier }) => {
+          if (redirect) router.push(Routes.AdminItemsItem({ identifier }))
+        })
+      )()
 
     return { onSubmit }
   },
--- app/items/mutations/createItem.ts
+++ app/items/mutations/createItem.ts
@@ -1,21 +1,79 @@
+import { Ctx } from "@blitzjs/next"
 import { resolver } from "@blitzjs/rpc"
-import { getUserRestaurant } from "app/auth/helpers/getUserRestaurant"
+import * as TE from "fp-ts/TaskEither"
+import * as T from "fp-ts/Task"
+import * as E from "fp-ts/Either"
+import * as A from "fp-ts/Array"
+import * as NEA from "fp-ts/NonEmptyArray"
 import { getBlurDataUrl } from "app/core/helpers/plaiceholder"
-import db from "db"
+import db, { Prisma, Venue } from "db"
 import { CreateItem } from "../validations"
+import { pipe, constant } from "fp-ts/function"
+import { PrismaValidationError } from "app/core/type/prisma"
+import { fpLog } from "app/core/helpers/server"
+import { z } from "zod"
+
+type NoOrgIdError = {
+  tag: "noOrgIdError"
+}
+
+type NoVenuesError = {
+  tag: "noVenuesError"
+  orgId: number
+}
+
+const constNoOrg = constant<NoOrgIdError>({ tag: "noOrgIdError" })
+
+const headFromArray =
+  <E>(err: E) =>
+  <V>(arr: V[]): E.Either<E, V> =>
+    A.isNonEmpty(arr) ? E.right(NEA.head(arr)) : E.left(err)
+
+const getFirstVenue = (ctx: Ctx) =>
+  pipe(
+    ctx.session.orgId,
+    E.fromNullable(constNoOrg()),
+    E.bindTo("orgId"),
+    TE.fromEither,
+    TE.bind("venues", ({ orgId }) =>
+      TE.fromTask(() => db.venue.findMany({ where: { organizationId: orgId } }))
+    ),
+    TE.bindW("venue", ({ venues, orgId }) =>
+      TE.fromEither(headFromArray<NoVenuesError>({ tag: "noVenuesError", orgId })(venues))
+    )
+  )
+
+interface CreateItemContext {
+  venues: Venue[]
+  venue: Venue
+  orgId: number
+  input: z.infer<typeof CreateItem>
+}
+
+const createItem = ({ input, orgId, venue }: CreateItemContext) =>
+  TE.tryCatch(
+    () =>
+      db.item.create({
+        data: { ...input, organizationId: orgId, venueId: venue.id, restaurantId: -1 },
+        include: {
+          content: true,
+        },
+      }),
+    (err): PrismaValidationError => ({
+      tag: "prismaValidationError",
+      error: err as Prisma.PrismaClientValidationError,
+    })
+  )
 
 export default resolver.pipe(
   resolver.zod(CreateItem),
   resolver.authorize(),
   async (input) => ({ ...input, blurDataUrl: await getBlurDataUrl(input.image) }),
-  async (input, ctx) => {
-    const restaurant = await getUserRestaurant(ctx)
-
-    return db.item.create({
-      data: { ...input, organizationId: ctx.session.orgId, restaurantId: restaurant.id },
-      include: {
-        content: true,
-      },
-    })
-  }
+  (input, ctx) =>
+    pipe(
+      getFirstVenue(ctx),
+      TE.bind("input", () => TE.of(input)),
+      TE.chainW(createItem),
+      TE.chainFirstIOK(fpLog.log)
+    )()
 )
