de9ce5074b5e032ccd55296b08b29694e7b5b355
Yuval Datner <contact@datner.me>
2022-08-27 05:40:06 +0300

    feat(items): add eqItem

---
+++ app/items/helpers/eqItem.ts
@@ -0,0 +1,24 @@
+import * as Eq from "fp-ts/Eq"
+import * as N from "fp-ts/number"
+import * as S from "fp-ts/string"
+import * as D from "fp-ts/Date"
+import { Item } from "@prisma/client"
+
+const eqDateOrNull: Eq.Eq<Date | null> = {
+  equals: (a, b) => (a !== null && b !== null && D.Eq.equals(a, b)) || a === b,
+}
+
+export const eqItem = Eq.struct<Item>({
+  id: N.Eq,
+  identifier: S.Eq,
+  image: S.Eq,
+  organizationId: N.Eq,
+  restaurantId: Eq.eqStrict,
+  categoryId: N.Eq,
+  venueId: Eq.eqStrict,
+  price: N.Eq,
+  updatedAt: D.Eq,
+  createdAt: D.Eq,
+  deleted: eqDateOrNull,
+  blurDataUrl: Eq.eqStrict,
+})
