24adfdd4bf374929b699af770b3fc65f188980c8
Yuval Datner <contact@datner.me>
2022-08-30 20:48:03 +0300

    feat(integrations): add helpers

--- integrations/helpers.ts
+++ integrations/helpers.ts
@@ -1,8 +1,11 @@
 import { foldMap } from "fp-ts/Array"
 import { tryCatch } from "fp-ts/TaskEither"
 import { MonoidSum } from "fp-ts/number"
-import db from "db"
+import db, { ClearingProvider as ClearingKind } from "db"
 import { prismaNotFound } from "app/core/helpers/prisma"
+import { match } from "ts-pattern"
+import { ClearingProvider } from "./clearingProvider"
+import { ReaderTask } from "fp-ts/ReaderTask"
 
 export type ReduceableItem = {
   price: number
@@ -13,3 +16,15 @@ export const getAmount: GetAmount = foldMap(MonoidSum)((it) => it.quantity * it.
 
 export const getOrder = (txId: string) =>
   tryCatch(() => db.order.findUniqueOrThrow({ where: { txId } }), prismaNotFound)
+
+export const getCreditGuardProvider = () =>
+  import("integrations/creditGuard/provider").then((mod) => mod.default)
+
+export const getPayPlusProvider = () =>
+  import("integrations/creditGuard/provider").then((mod) => mod.default)
+
+export const getClearingProvider: ReaderTask<ClearingKind, ClearingProvider> = (clearing) => () =>
+  match(clearing)
+    .with(ClearingKind.CREDIT_GUARD, getCreditGuardProvider)
+    .with(ClearingKind.PAY_PLUS, getPayPlusProvider)
+    .exhaustive()
