90612cf49555a7c595b735770fcd5858a42cfdd4
Yuval Datner <contact@datner.me>
2022-08-30 20:49:10 +0300

    refactor(menu): convert orderSuccess to fp

--- app/menu/mutations/orderSuccess.ts
+++ app/menu/mutations/orderSuccess.ts
@@ -1,21 +1,10 @@
 import { resolver } from "@blitzjs/rpc"
 import { z } from "zod"
-import {
-  CreditGuardIntegrationData,
-  // CreditGuardValidateOptions,
-  // validateCreditGuard,
-} from "app/menu/integrations/creditGuard"
-import db from "db"
-import * as TE from "fp-ts/TaskEither"
-// import * as IO from "fp-ts/IO"
-import * as E from "fp-ts/Either"
-// import * as T from "fp-ts/Task"
-import { sendOrder } from "integrations/dorix"
-import { constant, pipe } from "fp-ts/function"
-import { Venue } from "@prisma/client"
-import { zodParse } from "app/core/helpers/zod"
-import { getVenueById, prismaNotFound } from "app/core/helpers/prisma"
-import { creditGuardProvider } from "integrations/creditGuard/provider"
+import * as RTE from "fp-ts/ReaderTaskEither"
+import { pipe } from "fp-ts/function"
+import { getVenueById } from "app/core/helpers/prisma"
+import { getClearingProvider } from "integrations/helpers"
+import { ClearingProvider } from "integrations/clearingProvider"
 
 const OrderSuccess = z
   .object({
@@ -27,95 +16,24 @@ const OrderSuccess = z
     id: it.userData1,
   }))
 
-type IntegrationNotFoundError = {
-  tag: "integrationNotFoundError"
-  venue: Venue
-}
-
-type CreditGuardOrderNotFoundError = {
-  tag: "creditGuardOrderNotFoundError"
-  txId: string
-}
-
-const creditGuardOrderNotFound = (txId: string) =>
-  E.fromPredicate<number, CreditGuardOrderNotFoundError>(
-    (orderId) => orderId > -1,
-    constant({ tag: "creditGuardOrderNotFoundError", txId })
-  )
-
-// const validateCreditGuardTask =
-//   (opts: CreditGuardValidateOptions): T.Task<number> =>
-//   () =>
-//     validateCreditGuard(opts)
-
-// const toClearCardParams = ({
-//   venue,
-//   input,
-// }: {
-//   venue: Venue & { clearingIntegration: ClearingIntegration }
-//   input: z.infer<typeof OrderSuccess>
-// }) =>
-//   pipe(
-//     venue.clearingIntegration,
-//     E.fromNullable<IntegrationNotFoundError>({ tag: "integrationNotFoundError", venue }),
-//     E.chainW((integration) =>
-//       pipe(
-//         integration.vendorData,
-//         zodParse(CreditGuardIntegrationData),
-//         E.map(
-//           (vd): GetStatusParams => ({
-//             ...vd,
-//             terminal: integration.terminal,
-//             venueId: integration.venueId,
-//             txId: input.txId,
-//           })
-//         )
-//       )
-//     )
-//   )
+type OrderSuccess = z.infer<typeof OrderSuccess>
 
 const getVenueWithIntegrations = getVenueById({
   clearingIntegration: true,
   managementIntegration: true,
 })
 
-export default resolver.pipe(resolver.zod(OrderSuccess), ({ id, txId }) => true)
+const validate = (provider: ClearingProvider) =>
+  pipe(
+    RTE.asks<OrderSuccess, string>((i) => i.txId),
+    RTE.chainTaskK(provider.validateTransaction)
+  )
 
-// export default resolver.pipe(resolver.zod(OrderSuccess), ({ id, txId }) =>
-//   pipe(
-//     getVenueWithIntegrations(id),
-//     TE.chainEitherKW((venue) =>
-//       pipe(
-//         venue.clearingIntegration,
-//         E.fromNullable<IntegrationNotFoundError>({ tag: "integrationNotFoundError", venue }),
-//         E.chainW((integration) =>
-//           pipe(
-//             integration.vendorData,
-//             zodParse(CreditGuardIntegrationData),
-//             E.map(
-//               (vd): GetStatusParams => ({
-//                 ...vd,
-//                 terminal: integration.terminal,
-//                 venueId: integration.venueId,
-//                 txId,
-//               })
-//             )
-//           )
-//         )
-//       )
-//     ),
-//     TE.chainW(getStatus),
-//     TE.chainEitherKW(creditGuardOrderNotFound(txId)),
-//     TE.chainW((orderId) =>
-//       TE.tryCatch(
-//         () => db.order.findUniqueOrThrow({ where: { id: orderId }, include: { items: true } }),
-//         prismaNotFound
-//       )
-//     ),
-//     TE.chainTaskK(sendOrder),
-//     TE.match(
-//       ({ tag }) => "oh no, " + tag,
-//       () => "oh yeah"
-//     )
-//   )()
-// )
+const validateTransaction = pipe(
+  RTE.ask<OrderSuccess>(),
+  RTE.chainTaskEitherKW(({ id }) => getVenueWithIntegrations(id)),
+  RTE.chainTaskK((venue) => getClearingProvider(venue.clearingProvider)),
+  RTE.chainReaderTaskKW(validate)
+)
+
+export default resolver.pipe(resolver.zod(OrderSuccess), validateTransaction, (task) => task())
