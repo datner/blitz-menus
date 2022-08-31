d1f9cde6ed93a817c791536e828c9706296ca1e6
Yuval Datner <contact@datner.me>
2022-08-31 16:18:12 +0300

    feat(menu): add a bit more juice

--- app/menu/components/ItemData.tsx
+++ app/menu/components/ItemData.tsx
@@ -5,6 +5,9 @@ import { flow } from "fp-ts/function"
 import * as O from "fp-ts/Option"
 import { useTranslations } from "next-intl"
 import { memo, useEffect, useRef } from "react"
+import { ResizeObserver } from "@juggle/resize-observer"
+import useMeasure from "react-use-measure"
+import { useIsRtl } from "app/core/hooks/useIsRtl"
 
 type Props = {
   price: number
@@ -12,13 +15,17 @@ type Props = {
   amount: O.Option<number>
 }
 
-const reveal = flow(O.isSome, (it) => (it ? 1 : 0))
-
 const orOne = O.getOrElse(() => 1)
 
 export const ItemData = memo(function ItemData(props: Props) {
   const { price, content, amount } = props
-  const { opacity } = useSpring({ opacity: reveal(amount) })
+  const [ref, { width }] = useMeasure({ polyfill: ResizeObserver })
+  const isRtl = useIsRtl()
+  const rtlWidth = isRtl ? width : -width
+  const { opacity, x } = useSpring({
+    opacity: O.isSome(amount) ? 1 : 0,
+    x: O.isSome(amount) ? 0 : rtlWidth,
+  })
   const t = useTranslations("menu.Components.ItemData")
   const amountRef = useRef(1)
   useEffect(() => {
@@ -28,13 +35,19 @@ export const ItemData = memo(function ItemData(props: Props) {
   const orPrev = O.getOrElse(() => amountRef.current)
 
   return (
-    <dl className="z-10 flex h-full flex-col truncate p-3">
+    <dl className="z-10 flex h-full flex-col p-3">
       <dt className="sr-only">{t("name")}</dt>
-      <dd className="text-sm sm:text-base text-gray-800">
-        {content.name}{" "}
-        <animated.span style={{ opacity }} className="font-semibold text-indigo-600">
-          x{orPrev(amount)}
-        </animated.span>
+      <dd className="text-sm sm:text-base text-gray-800 overflow-hidden">
+        <animated.div style={{ x }} className="flex flex-nowrap">
+          <animated.span
+            ref={ref}
+            style={{ opacity }}
+            className="font-semibold ltr:pr-1.5 rtl:pl-1.5 text-indigo-600"
+          >
+            x{orPrev(amount)}
+          </animated.span>
+          <animated.span className="block truncate">{content.name}</animated.span>
+        </animated.div>
       </dd>
       <dt className="sr-only">{t("description")}</dt>
       <dd className="text-xs sm:text-sm text-gray-500 whitespace-normal line-clamp-2 ">
