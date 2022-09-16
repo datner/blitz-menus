2ea0e45813955fb36d1d9963f0dd92e8a746ca7c
Yuval Datner <contact@datner.me>
2022-09-16 05:20:37 +0300

    fix(payments): apply temp fix

--- pages/payments/[provider]/success.tsx
+++ pages/payments/[provider]/success.tsx
@@ -4,7 +4,9 @@ import { useQuery } from "@blitzjs/rpc"
 import { CheckIcon, DotsHorizontalIcon, XIcon } from "@heroicons/react/solid"
 import orderSuccess from "app/menu/queries/orderSuccess"
 import { ClearingProvider } from "@prisma/client"
-import { match } from "ts-pattern"
+import { pipe } from "fp-ts/function"
+import { match } from "fp-ts/Either"
+import * as O from "fp-ts/Option"
 
 export default function Success() {
   const provider = useParam("provider") as ClearingProvider
@@ -17,56 +19,70 @@ export default function Success() {
 
   return (
     <div className="h-full flex items-center">
-      {match(update)
-        .with({ right: "VALID" }, () => (
-          <div>
-            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-              <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-            </div>
-            <div className="mt-3 text-center sm:mt-5">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">Payment successful!</h3>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                  Thank you for using Renu, we are still in very early rollout. We know we have
-                  something to say about the service, please tell us here: [textbox]
-                </p>
+      {pipe(
+        O.fromNullable(update),
+        O.match(
+          () => (
+            <div>
+              <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-blue-100">
+                <DotsHorizontalIcon className="h-6 w-6 text-blue-600" aria-hidden="true" />
+              </div>
+              <div className="mt-3 text-center sm:mt-5">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">Wait for it....</h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    {`We're just checking to see everything is fine and telling the kitchen to start cooking!`}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))
-        .with({ right: "INVALID" }, () => (
-          <div>
-            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
-              <XIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-            </div>
-            <div className="mt-3 text-center sm:mt-5">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">Payment Error!</h3>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                  {`Oops! We are very sorry, but something went wrong and your order didn't get through.
+          ),
+          (o) =>
+            pipe(
+              o,
+              match(
+                () => (
+                  <div>
+                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                      <XIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <h3 className="text-lg leading-6 font-medium text-gray-900">
+                        Payment Error!
+                      </h3>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          {`Oops! We are very sorry, but something went wrong and your order didn't get through.
               Don't worry, we notified our team and will resolve this shortly! Please be patient
 
               with us while we're still in early beta`}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))
-        .otherwise(() => (
-          <div>
-            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-blue-100">
-              <DotsHorizontalIcon className="h-6 w-6 text-blue-600" aria-hidden="true" />
-            </div>
-            <div className="mt-3 text-center sm:mt-5">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">Wait for it....</h3>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                  {`We're just checking to see everything is fine and telling the kitchen to start cooking!`}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ),
+                () => (
+                  <div>
+                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                      <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <h3 className="text-lg leading-6 font-medium text-gray-900">
+                        Payment successful!
+                      </h3>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Thank you for using Renu, we are still in very early rollout. We know we
+                          have something to say about the service, please tell us here: [textbox]
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                )
+              )
+            )
+        )
+      )}
     </div>
   )
 }
