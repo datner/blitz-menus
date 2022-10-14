73565181f3b57d85ebb9b6428eb2364949c1bf80
Yuval Datner <contact@datner.me>
2022-10-15 01:06:10 +0300

    refactor(layouts): change to mantine app shell

--- app/core/layouts/AdminLayout.tsx
+++ app/core/layouts/AdminLayout.tsx
@@ -1,189 +1,81 @@
 import { BlitzLayout } from "@blitzjs/next"
 import clsx from "clsx"
-import { Dialog, Transition } from "@headlessui/react"
-import { createContext, Fragment, ReactNode, Suspense, useContext, useReducer } from "react"
+import { ReactNode, Suspense } from "react"
 import Layout from "./Layout"
 import {
+  Square2StackIcon,
   CogIcon,
-  CollectionIcon,
+  CircleStackIcon as CollectionIcon,
   HomeIcon,
-  PhotographIcon,
+  PhotoIcon as PhotographIcon,
   UserGroupIcon,
-  ViewGridIcon,
-  XIcon,
-} from "@heroicons/react/solid"
+} from "@heroicons/react/24/solid"
 import { ImpersonationNotice } from "app/auth/components/ImpersonationNotice"
-import { useEvent } from "../hooks/useEvent"
 import { ToastContainer } from "react-toastify"
 import NiceModal from "@ebay/nice-modal-react"
+import { useIsRtl } from "../hooks/useIsRtl"
+import { AppShell, Navbar } from "@mantine/core"
 
 import "react-toastify/dist/ReactToastify.css"
-import { useIsRtl } from "../hooks/useIsRtl"
+import { ActiveLink } from "../components/ActiveLink"
 
 type Props = { children?: ReactNode }
 
-const layoutContent = createContext(() => {})
-export const useMobileMenu = () => useContext(layoutContent)
+const sidebarNavigation = [
+  { name: "Home", href: "/admin/home", icon: HomeIcon, current: false },
+  { name: "Inventory", href: "/admin/items", icon: Square2StackIcon, current: false },
+  { name: "Photos", href: "/admin/photos", icon: PhotographIcon, current: true },
+  { name: "Shared", href: "/admin/shared", icon: UserGroupIcon, current: false },
+  { name: "Albums", href: "/admin/albums", icon: CollectionIcon, current: false },
+  { name: "Settings", href: "/admin/settings", icon: CogIcon, current: false },
+]
 
-export const AdminLayout: BlitzLayout<Props> = ({ children }) => {
-  const [mobileMenuShow, toggleMobileMenu] = useReducer((s) => !s, false)
-  const isRtl = useIsRtl()
-  return (
-    <Layout title="Renu | Admin">
+const navLinks = sidebarNavigation.map((item) => (
+  <ActiveLink key={item.name} href={item.href}>
+    {({ active }) => (
+      <a
+        className={clsx(
+          active
+            ? "bg-emerald-800 text-white"
+            : "text-emerald-100 hover:bg-emerald-800 hover:text-white",
+          "group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium"
+        )}
+        aria-current={active ? "page" : undefined}
+      >
+        <item.icon
+          className={clsx(
+            active ? "text-white" : "text-emerald-300 group-hover:text-white",
+            "h-6 w-6"
+          )}
+          aria-hidden="true"
+        />
+        <span className="mt-2">{item.name}</span>
+      </a>
+    )}
+  </ActiveLink>
+))
+
+const navigation = (
+  <Navbar sx={(theme) => ({ backgroundColor: theme.colors.green[7] })} width={{ sm: 112 }}>
+    <Navbar.Section>
+      <div className="h-16"></div>
+    </Navbar.Section>
+    <Navbar.Section grow className="p-3 space-y-2">
+      {navLinks}
+    </Navbar.Section>
+  </Navbar>
+)
+
+export const AdminLayout: BlitzLayout<Props> = ({ children }) => (
+  <Layout title="Renu | Admin">
+    <AppShell className="h-full" classNames={{ body: "h-full" }} padding={0} navbar={navigation}>
       <Suspense>
         <ImpersonationNotice />
       </Suspense>
       <NiceModal.Provider>
-        <div className="h-full flex">
-          <layoutContent.Provider value={useEvent(() => toggleMobileMenu())}>
-            <NarrowSidebar />
-            <MobileMenu show={mobileMenuShow} onHide={toggleMobileMenu} />
-            {children}
-          </layoutContent.Provider>
-        </div>
-        <ToastContainer rtl={isRtl} autoClose={1500} position="bottom-right" />
+        <div className="h-full flex">{children}</div>
+        <ToastContainer rtl={useIsRtl()} autoClose={1500} position="bottom-right" />
       </NiceModal.Provider>
-    </Layout>
-  )
-}
-const sidebarNavigation = [
-  { name: "Home", href: "#", icon: HomeIcon, current: false },
-  { name: "All Files", href: "#", icon: ViewGridIcon, current: false },
-  { name: "Photos", href: "#", icon: PhotographIcon, current: true },
-  { name: "Shared", href: "#", icon: UserGroupIcon, current: false },
-  { name: "Albums", href: "#", icon: CollectionIcon, current: false },
-  { name: "Settings", href: "#", icon: CogIcon, current: false },
-]
-
-function NarrowSidebar() {
-  return (
-    <div className="hidden w-28 bg-indigo-700 overflow-y-auto md:block">
-      <div className="w-full py-6 flex flex-col items-center">
-        <div className="flex-shrink-0 flex items-center">
-          <img
-            className="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
-            alt="Workflow"
-          />
-        </div>
-        <div className="flex-1 mt-6 w-full px-2 space-y-1">
-          {sidebarNavigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className={clsx(
-                item.current
-                  ? "bg-indigo-800 text-white"
-                  : "text-indigo-100 hover:bg-indigo-800 hover:text-white",
-                "group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium"
-              )}
-              aria-current={item.current ? "page" : undefined}
-            >
-              <item.icon
-                className={clsx(
-                  item.current ? "text-white" : "text-indigo-300 group-hover:text-white",
-                  "h-6 w-6"
-                )}
-                aria-hidden="true"
-              />
-              <span className="mt-2">{item.name}</span>
-            </a>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
-
-function MobileMenu({ show, onHide }: { show: boolean; onHide(): void }) {
-  return (
-    <Transition.Root show={show} as={Fragment}>
-      <Dialog as="div" className="relative z-20 md:hidden" onClose={onHide}>
-        <Transition.Child
-          as={Fragment}
-          enter="transition-opacity ease-linear duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity ease-linear duration-300"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-40 flex">
-          <Transition.Child
-            as={Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
-            <Dialog.Panel className="relative max-w-xs w-full bg-indigo-700 pt-5 pb-4 flex-1 flex flex-col">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-300"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="absolute top-1 right-0 -mr-14 p-1">
-                  <button
-                    type="button"
-                    className="h-12 w-12 rounded-full flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-white"
-                    onClick={onHide}
-                  >
-                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                    <span className="sr-only">Close sidebar</span>
-                  </button>
-                </div>
-              </Transition.Child>
-              <div className="flex-shrink-0 px-4 flex items-center">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
-                  alt="Workflow"
-                />
-              </div>
-              <div className="mt-5 flex-1 h-0 px-2 overflow-y-auto">
-                <nav className="h-full flex flex-col">
-                  <div className="space-y-1">
-                    {sidebarNavigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={clsx(
-                          item.current
-                            ? "bg-indigo-800 text-white"
-                            : "text-indigo-100 hover:bg-indigo-800 hover:text-white",
-                          "group py-2 px-3 rounded-md flex items-center text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        <item.icon
-                          className={clsx(
-                            item.current ? "text-white" : "text-indigo-300 group-hover:text-white",
-                            "mr-3 h-6 w-6"
-                          )}
-                          aria-hidden="true"
-                        />
-                        <span>{item.name}</span>
-                      </a>
-                    ))}
-                  </div>
-                </nav>
-              </div>
-            </Dialog.Panel>
-          </Transition.Child>
-          <div className="flex-shrink-0 w-14" aria-hidden="true">
-            {/* Dummy element to force sidebar to shrink to fit close icon */}
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
-  )
-}
+    </AppShell>
+  </Layout>
+)
