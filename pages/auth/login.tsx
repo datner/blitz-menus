34fb691e4b43a913353a46a74c8846e0c50f016f
Yuval Datner <contact@datner.me>
2022-10-15 01:15:49 +0300

    feat(auth): redo login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,15 +1,7 @@
 import { BlitzPage, Routes } from "@blitzjs/next"
 import { useRouter } from "next/router"
-import { PromiseReturnType } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import { LoginForm } from "app/auth/components/LoginForm"
-import login from "app/auth/mutations/login"
-
-function getRoute(user: PromiseReturnType<typeof login>) {
-  if (user.restaurant) return Routes.AdminHome({ restaurant: user.restaurant.slug })
-
-  return Routes.RestaurantSignupPage()
-}
 
 const LoginPage: BlitzPage = () => {
   const router = useRouter()
@@ -18,10 +10,10 @@ const LoginPage: BlitzPage = () => {
     <div className="min-h-full flex bg-gray-50 flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <LoginForm
-          onSuccess={(user) => {
+          onSuccess={() => {
             const next = router.query.next
               ? decodeURIComponent(router.query.next as string)
-              : getRoute(user)
+              : Routes.AdminHome()
 
             router.push(next)
           }}
@@ -31,7 +23,7 @@ const LoginPage: BlitzPage = () => {
   )
 }
 
-LoginPage.redirectAuthenticatedTo = "/"
+LoginPage.redirectAuthenticatedTo = Routes.AdminHome()
 LoginPage.getLayout = (page) => <Layout title="Log In">{page}</Layout>
 
 export default LoginPage
