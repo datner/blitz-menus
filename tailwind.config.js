96f0ede2199ed21f4875b98d11dc98afce05e47f
Yuval Datner <contact@datner.me>
2022-10-18 01:15:39 +0300

    feat(tailwind): change form strategy to class

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
   },
   plugins: [
-    require("@tailwindcss/forms"),
+    require("@tailwindcss/forms")({ strategy: "class" }),
     require("@tailwindcss/line-clamp"),
     plugin(function ({ addVariant }) {
       addVariant("error", ["&[aria-invalid=true]", "&:invalid"])
