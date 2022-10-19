a2691f6376a0c115d26e88cfaf8e657da5c2767c
Yuval Datner <contact@datner.me>
2022-10-19 20:49:18 +0300

    feat(_document): add fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,12 @@ class MyDocument extends Document {
     const dir = locale === Locale.he ? "rtl" : "ltr"
     return (
       <Html lang={locale} dir={dir} className="h-full bg-white">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Secular+One&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body className="h-full">
           <Main />
           <NextScript />
