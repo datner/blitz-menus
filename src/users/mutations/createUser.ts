f28ba1cf815c6e672d594c183214af3b2e36784e
Yuval Datner <contact@datner.me>
2022-11-14 13:08:38 +0200

    feat(users): add create user

---
+++ src/users/mutations/createUser.ts
@@ -0,0 +1,15 @@
+import { SecurePassword } from "@blitzjs/auth"
+import { resolver } from "@blitzjs/rpc"
+import { GlobalRole } from "@prisma/client"
+import { CreateClientSchema } from "../validations"
+
+export default resolver.pipe(
+  resolver.zod(CreateClientSchema),
+  resolver.authorize(GlobalRole.SUPER),
+  async ({ password, email }) => {
+    const hashedPassword = await SecurePassword.hash(password.trim())
+    return db.user.create({
+      data: { email: email.toLowerCase().trim(), hashedPassword, role: GlobalRole.USER },
+    })
+  }
+)
