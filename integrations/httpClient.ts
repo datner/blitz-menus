6bf87afcc7b8b06695e5f6133e083c855886f723
Yuval Datner <contact@datner.me>
2022-11-17 11:03:46 +0200

    refactor(integrations): remove dead code

--- integrations/httpClient.ts
+++ integrations/httpClient.ts
@@ -3,7 +3,6 @@ import { flow, identity, pipe } from "fp-ts/function"
 import * as RTE from "fp-ts/ReaderTaskEither"
 import * as TE from "fp-ts/TaskEither"
 import * as E from "fp-ts/Either"
-import { z } from "zod"
 
 export type HttpError = HttpRequestError | HttpResponseStatusError | AxiosRequestError
 
@@ -15,11 +14,6 @@ export type HttpRequestError = {
 export type AxiosRequestError = {
   tag: "axiosRequestError"
   error: AxiosError
-}
-
-export type ZodParseError = {
-  tag: "zodParseError"
-  error: z.ZodError
 }
 
 export type HttpResponseContentTypeError = {
@@ -71,10 +65,3 @@ export const ensureStatus =
     min <= response.status && response.status < max
       ? E.right(response)
       : E.left({ tag: "httpResponseStatusError", status: response.status })
-
-export const toTyped =
-  <Schema extends z.ZodTypeAny>(schema: Schema) =>
-  (response: AxiosResponse): E.Either<ZodParseError, z.output<Schema>> =>
-    pipe(schema.safeParse(response.data), (result) =>
-      result.success ? E.right(result.data) : E.left({ tag: "zodParseError", error: result.error })
-    )
