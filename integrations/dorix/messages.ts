372baa9f42397589c114cccc568c1b793eef7eb4
Yuval Datner <contact@datner.me>
2022-11-17 11:04:29 +0200

    refactor(dorix): redo messages to new Format format

--- integrations/dorix/messages.ts
+++ integrations/dorix/messages.ts
@@ -1,10 +1,11 @@
 import { Order } from "@prisma/client"
 import { NoEnvVarError } from "src/core/helpers/env"
 import { Json } from "fp-ts/Json"
-import { AxiosRequestError, HttpResponseStatusError, ZodParseError } from "integrations/httpClient"
+import { AxiosRequestError, HttpResponseStatusError } from "integrations/httpClient"
 import { sendMessage } from "integrations/telegram/sendMessage"
 import { DorixResponseError } from "./client"
 import { Format } from "telegraf"
+import { ZodParseError } from "src/core/helpers/zod"
 
 export const reportOrderAxiosError = (order: Order) => (e: AxiosRequestError) =>
   sendMessage(
@@ -17,68 +18,83 @@ export const reportOrderAxiosError = (order: Order) => (e: AxiosRequestError) =>
   )
 
 export const reportOrderResponseStatusError = (order: Order) => (e: HttpResponseStatusError) =>
-  sendMessage(`
-Oh no, Dorix returned \`${e.status}\` for our request to update order ${order.id} of venue ${order.venueId}\\.
-`)
+  sendMessage(
+    Format.fmt(
+      `Oh no, Dorix returned`,
+      Format.code(e.status.toString()),
+      `for our request to update order ${order.id} of venue ${order.venueId}.`
+    )
+  )
 
 export const reportOrderZodError = (order: Order) => (e: ZodParseError) =>
-  sendMessage(`
-Thats weird. Dorix payload came back malformed for our request to update order ${order.id} of venue ${order.venueId}\\.
-
-Error details:
-\`\`\`
-${e.error.message}
-\`\`\`
-`)
+  sendMessage(
+    Format.fmt(
+      `Thats weird. Dorix payload came back malformed for our request to update order ${order.id} of venue ${order.venueId}.`,
+      "\n\n",
+      "Error details:\n",
+      Format.code(e.error.message),
+      "\nPayload Recieved:\n",
+      Format.code(e.tag)
+    )
+  )
 
 export const reportDorixOrderError = (order: Order) => (e: DorixResponseError) =>
-  sendMessage(`
-Dorix didn't acknowledge our request to update order ${order.id} of venue ${order.venueId}\\.
+  sendMessage(
+    Format.fmt(
+      `
+Dorix didn't acknowledge our request to update order ${order.id} of venue ${order.venueId}.
 
-Error details:
-\`\`\`
-${e.message}
-\`\`\`
-`)
+Error details:\n`,
+      Format.code(e.message)
+    )
+  )
 
 export const reportOrderSuccess = (order: Order) => () =>
   sendMessage(`Hooray\\! ${order.id} was succesfully reported to Dorix\\!`)
 
 export const reportStatusAxiosError = (params: Order) => (e: AxiosRequestError) =>
-  sendMessage(`
+  sendMessage(
+    Format.fmt(
+      `
 Oh no, we couldn't reach Dorix to get status of order ${params.id}
 
-Error details:
-\`\`\`
-${e.error.message}
-\`\`\`
-`)
+Error details:\n`,
+      Format.code(e.error.message)
+    )
+  )
 
 export const reportStatusResponseStatusError = (params: Order) => (e: HttpResponseStatusError) =>
   sendMessage(`
-Oh no, Dorix returned \`${e.status}\` for our request to get status of order ${params.id}\\.
+Oh no, Dorix returned \`${e.status}\` for our request to get status of order ${params.id}.
 `)
 
 export const reportGenericError = (details: Json) =>
-  sendMessage(`
-Generic Error Caught, details below. I hope it's not long\\.\\.
-
-\`\`\`
-${JSON.stringify(details, null, 2)}
-\`\`\`
-`)
+  sendMessage(
+    Format.fmt(
+      `
+Generic Error Caught, details below. I hope it's not long..
+\n\n`,
+      Format.pre("json")(JSON.stringify(details, null, 2))
+    )
+  )
 
 export const reportStatusZodError = (params: Order) => (e: ZodParseError) =>
-  sendMessage(`
-Thats weird. Dorix payload came back malformed for our request to get status for order ${params.id}\\.
+  sendMessage(
+    Format.fmt(
+      `
+Thats weird. Dorix payload came back malformed for our request to get status for order ${params.id}.
 
-Error details:
-\`\`\`
-${e.error.message}
-\`\`\`
-`)
+Error details:\n`,
+      Format.code(e.error.message)
+    )
+  )
 
 export const reportEnvVarError = (e: NoEnvVarError) =>
-  sendMessage(`
-Dorix request failed prematurely due to a missing env var \`${e.key}\`\\!
-`)
+  sendMessage(
+    Format.fmt(
+      `
+Dorix request failed prematurely due to a missing env var`,
+      Format.code(String(e.key)),
+      "!"
+    )
+  )
