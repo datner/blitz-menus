05561d73f71ad2116d7b4a40597a5b1f6afb683e
Yuval Datner <contact@datner.me>
2022-12-04 01:00:31 +0200

    chore(npm): add got

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,18 @@
 
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      integrations: resolve(__dirname, "./integrations"),
+      src: resolve(__dirname, "./src"),
+      pages: resolve(__dirname, "./pages"),
+    },
+  },
   test: {
     environment: "jsdom",
   },
